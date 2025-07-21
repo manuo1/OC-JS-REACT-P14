5bd2bddfa982ab852ee1ffc3a9d9a306e3cfc8eb
Emmanuel Oudot <manuo1@free.fr>
2025-07-21 14:58:06 +0200

    feat(store): add Zustand store with employee state

---
+++ src/store/useEmployeeStore.js
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+const useEmployeeStore = create((set) => ({
+  employees: [],
+  addEmployee: (employee) =>
+    set((state) => ({
+      employees: [...state.employees, employee],
+    })),
+}));
+
+export default useEmployeeStore;
