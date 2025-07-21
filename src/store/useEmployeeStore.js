dbb5f55cabb318da4970e2c7a2f2d62cd9d9cd8b
Emmanuel Oudot <manuo1@free.fr>
2025-07-21 15:05:51 +0200

    feat(store): persist employees store in localStorage

--- src/store/useEmployeeStore.js
+++ src/store/useEmployeeStore.js
@@ -1,11 +1,19 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useEmployeeStore = create((set) => ({
-  employees: [],
-  addEmployee: (employee) =>
-    set((state) => ({
-      employees: [...state.employees, employee],
-    })),
-}));
+const useEmployeeStore = create(
+  persist(
+    (set) => ({
+      employees: [],
+      addEmployee: (employee) =>
+        set((state) => ({
+          employees: [...state.employees, employee],
+        })),
+    }),
+    {
+      name: "hrnet-employees", // localStorage name
+    }
+  )
+);
 
 export default useEmployeeStore;
