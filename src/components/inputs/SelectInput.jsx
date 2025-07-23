29cff2ab40dd44946eee9aec42a5bc96393513cb
Emmanuel Oudot <manuo1@free.fr>
2025-07-23 20:28:08 +0200

    style: modernize appearance of TextInput and SelectInput components

--- src/components/inputs/SelectInput.jsx
+++ src/components/inputs/SelectInput.jsx
@@ -3,9 +3,21 @@ import styles from "./SelectInput.module.scss";
 function SelectInput({ label, name, value, onChange, options, error }) {
   return (
     <div className={styles.field}>
-      <label htmlFor={name}>{label}</label>
-      <select id={name} name={name} value={value} onChange={onChange}>
-        <option value="">-- Select --</option>
+      <label htmlFor={name} className={styles.label}>
+        {label}
+      </label>
+      <select
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className={`${styles.select} ${error ? styles.invalid : ""}`}
+        aria-invalid={!!error}
+        required
+      >
+        <option value="" disabled hidden>
+          -- Select an option --
+        </option>
         {options.map((opt) => (
           <option key={opt} value={opt}>
             {opt}
--- src/components/inputs/TextInput.jsx
+++ src/components/inputs/TextInput.jsx
@@ -3,13 +3,18 @@ import styles from "./TextInput.module.scss";
 function TextInput({ label, name, value, onChange, error, type = "text" }) {
   return (
     <div className={styles.field}>
-      <label htmlFor={name}>{label}</label>
+      <label htmlFor={name} className={styles.label}>
+        {label}
+      </label>
       <input
         id={name}
         name={name}
         type={type}
         value={value}
         onChange={onChange}
+        className={`${styles.input} ${error ? styles.invalid : ""}`}
+        aria-invalid={!!error}
+        required
       />
       {error && <span className={styles.error}>{error}</span>}
     </div>
